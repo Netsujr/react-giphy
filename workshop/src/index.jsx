5aeff65e98f52691280231ee2110a77557829226
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-08 19:41:10 +0900

    added name

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,12 @@
 import '../assets/stylesheets/application.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+  const message = `Hello ${props.name} !`;
+  return <h1>{message}</h1>;
+};
+
+const root = document.getElementById('root');
+ReactDOM.render(<Hello name="Renato" />, root);

95ff415be9937e851b867560164ba170ac9bd4e5
Renato Delboni <renatonato_jr@hotmail.com>
2021-12-08 19:42:54 +0900

    created props const

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -4,9 +4,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Hello = (props) => {
-  const message = `Hello ${props.name} !`;
+  const { name, age } = props;
+  const message = `Hello, I'm ${props.name} and I am ${age} years old!`;
   return <h1>{message}</h1>;
 };
 
 const root = document.getElementById('root');
-ReactDOM.render(<Hello name="Renato" />, root);
+ReactDOM.render(<Hello name="Renato" age="28" />, root);
